b1ae72f123ec47cfdc00370a7eb2bc2b70f59a54
pinmeeee <tokkimee@gmail.com>
2020-09-12 18:36:37 +0800

    timer thingy

--- fettle/lib/ui/screens/exercise.dart
+++ fettle/lib/ui/screens/exercise.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import '../components/shared/bottomNavBar.dart';
 import '../../constants.dart';
+import '../components/exercise/timers/timer1.dart';
 
 class ExerciseScreen extends StatefulWidget {
   static String id = 'exercise';
@@ -11,6 +12,24 @@ class ExerciseScreen extends StatefulWidget {
 
 class _ExerciseScreenState extends State<ExerciseScreen> {
   bool loading = false;
+
+  void showBottomSheet(context) {
+    showModalBottomSheet(
+        isScrollControlled: true,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(25.0),
+            topLeft: Radius.circular(25.0),
+          ),
+        ),
+        context: context,
+        builder: (BuildContext context) {
+          return Container(
+            height: MediaQuery.of(context).size.height * 0.9,
+            child: timer1(),
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,11 +45,32 @@ class _ExerciseScreenState extends State<ExerciseScreen> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(),
-              BottomNavBar(currentScreenId: ExerciseScreen.id)
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Container(),
+                  Text('lets work out'),
+                  RaisedButton(
+                      onPressed: () => showBottomSheet(context),
+                      child: Text("Timer 1")
+                      // onPressed: () {
+                      //   Navigator.push(
+                      //     context,
+                      //     MaterialPageRoute(builder: (context) => timer1()),
+                      //   );
+                      // },
+                      // child: Text('Timer1'),
+                      ),
+                  Text('timer 2 button'),
+
+//need button to timer 2 here
+                ],
+              ),
+              BottomNavBar(currentScreenId: ExerciseScreen.id),
             ],
           ),
         ));
   }
 }
+
+// test test pls workkkk
