7682b2cda4ae68f1a577456b252cd4b727b5cf9f
Yifan <jeromeyao1999@gmail.com>
2020-09-15 22:45:33 +0800

    Playground Page Basic Layout

--- fettle/lib/ui/screens/exercise.dart
+++ fettle/lib/ui/screens/exercise.dart
@@ -48,20 +48,107 @@ class _ExerciseScreenState extends State<ExerciseScreen> {
             children: <Widget>[
               Column(
                 children: <Widget>[
-                  Container(),
-                  Text('lets work out'),
-                  RaisedButton(
-                      onPressed: () => showBottomSheet(context),
-                      child: Text("Timer 1")
-                      // onPressed: () {
-                      //   Navigator.push(
-                      //     context,
-                      //     MaterialPageRoute(builder: (context) => timer1()),
-                      //   );
-                      // },
-                      // child: Text('Timer1'),
+                  SizedBox(height: 10,),
+                  Text(
+                    'PLAYGROUND',
+                    style: TextStyle(
+                      fontSize: 42,
+                      letterSpacing: 3,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey[800]
+                    ),
+                  ),
+                  SizedBox(height: 30,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width * 0.40,
+                        margin: EdgeInsets.all(5.0),
+                        padding: EdgeInsets.all(3.0),
+                        alignment: Alignment.center,
+                        decoration: stepBoxDecoration(),
+                        child: Text(
+                          "Today's Step: 13345",
+                          style: TextStyle(
+                              fontSize: 22,
+                              letterSpacing: 1.5,
+                              color: Colors.grey[800]
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
                       ),
-                  Text('timer 2 button'),
+                      Container(
+                        width: MediaQuery.of(context).size.width * 0.40,
+                        margin: EdgeInsets.all(5.0),
+                        alignment: Alignment.center,
+                        decoration: goldenMouseBoxDecoration(),
+                        child: Container(
+                          color: Colors.redAccent,
+                          padding: EdgeInsets.all(3.0),
+                          child: Text(
+                            "Golden Mouse Event is On!",
+                            style: TextStyle(
+                                fontSize: 22,
+                                letterSpacing: 1.5,
+                                color: Colors.black
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 30,),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.90,
+                    height: MediaQuery.of(context).size.height * 0.30,
+                    alignment: Alignment.center,
+                    decoration: catAnimationBoxDecoration(),
+                    child: Text(
+                      'Cat in Exercise Animation',
+                      style: TextStyle(
+                        fontSize: 32,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  SizedBox(height: 30,),
+                  Text(
+                      "Let's work out!",
+                      style: TextStyle(
+                        fontSize: 22,
+                      ),
+                      textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 20,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(3.0),
+                        child: RaisedButton(
+                            onPressed: () => showBottomSheet(context),
+                            child: Text("Timer 1")
+                            // onPressed: () {
+                            //   Navigator.push(
+                            //     context,
+                            //     MaterialPageRoute(builder: (context) => timer1()),
+                            //   );
+                            // },
+                            // child: Text('Timer1'),
+                            ),
+                      ),
+                      Container(
+                          padding: EdgeInsets.all(3.0),
+                          child: RaisedButton(
+                              onPressed: () => showBottomSheet(context),
+                              child: Text("Timer 2")
+                          ),
+                      ),
+                    ],
+                  ),
+
 
 //need button to timer 2 here
                 ],
@@ -72,5 +159,36 @@ class _ExerciseScreenState extends State<ExerciseScreen> {
         ));
   }
 }
+
+BoxDecoration stepBoxDecoration(){
+  return BoxDecoration(
+    border: Border.all(
+      width: 2,
+      color: Colors.cyan[400],
+    )
+  );
+}
+
+BoxDecoration goldenMouseBoxDecoration(){
+  return BoxDecoration(
+      border: Border.all(
+        width: 5,
+        color: Colors.red,
+      ),
+      borderRadius: BorderRadius.all(Radius.circular(5.0)),
+  );
+}
+
+BoxDecoration catAnimationBoxDecoration(){
+  return BoxDecoration(
+    border: Border.all(
+      width: 2,
+      color: Colors.cyan[200],
+    ),
+    borderRadius: BorderRadius.all(Radius.circular(5.0)),
+
+  );
+}
+
 
 // test test pls workkkk
