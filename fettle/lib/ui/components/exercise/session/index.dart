3ec39d4d8be463125e621fb4d000928792e533e8
Rails Udemy Course <zonghangoh@gmail.com>
2020-10-05 20:45:41 +0800

    nvm old implementation more stable

--- fettle/lib/ui/components/exercise/session/index.dart
+++ fettle/lib/ui/components/exercise/session/index.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 //Agora stuff
-import 'package:agora_rtc_engine/rtc_engine.dart';
-import 'package:agora_rtc_engine/rtc_local_view.dart' as RtcLocalView;
-import 'package:agora_rtc_engine/rtc_remote_view.dart' as RtcRemoteView;
+import 'package:agora_rtc_engine/agora_rtc_engine.dart';
 
 // replace with your App ID from Agora.io
 const APP_ID = "aae49542fb7e4c98b20b970b8c9ebe00";
@@ -22,100 +20,300 @@ class VideoRoom extends StatefulWidget {
 }
 
 class VideoRoomState extends State<VideoRoom> {
-  bool _joined = false;
-  int _remoteUid;
-  bool _switch = false;
+  static final _users = List<int>();
+  final _infoStrings = <String>[];
+  bool muted = false;
+
+  @override
+  void dispose() {
+    // clear users
+    _users.clear();
+    // destroy sdk
+    AgoraRtcEngine.leaveChannel();
+    AgoraRtcEngine.destroy();
+    super.dispose();
+  }
 
   @override
   void initState() {
     super.initState();
-    initPlatformState();
+    init();
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    var engine = await RtcEngine.create(APP_ID);
-    engine.setEventHandler(RtcEngineEventHandler(
-        joinChannelSuccess: (String channel, int uid, int elapsed) {
-      print('joinChannelSuccess ${channel} ${uid}');
+  // initialize agora sdk
+  init() async {
+    initialize();
+  }
+
+  void initialize() {
+    if (APP_ID.isEmpty) {
       setState(() {
-        _joined = true;
+        _infoStrings
+            .add("APP_ID missing, please provide your APP_ID in settings.dart");
+        _infoStrings.add("Agora Engine is not starting");
       });
-    }, userJoined: (int uid, int elapsed) {
-      print('userJoined ${uid}');
-      setState(() {
-        _remoteUid = uid;
-      });
-    }, userOffline: (int uid, UserOfflineReason reason) {
-      print('userOffline ${uid}');
-      setState(() {
-        _remoteUid = null;
-      });
-    }));
-    await engine.enableVideo();
-    await engine.joinChannel(
+      return;
+    }
+
+    _initAgoraRtcEngine();
+    _addAgoraEventHandlers();
+    AgoraRtcEngine.enableWebSdkInteroperability(true);
+    // set parameters for Agora Engine
+    AgoraRtcEngine.setParameters('{\"che.video.lowBitRateStreamParameter\"' +
+        ':{\"width\":320,\"height\":180,\"frameRate\":15,\"bitRate\":140}}');
+    // join channel corresponding to current group
+    AgoraRtcEngine.joinChannel(
         '006aae49542fb7e4c98b20b970b8c9ebe00IAAHZ5H2j9BV/CErLRNm+u+tqRl4yz/xTa9vuZH+Eb6hK+Osgc1PMNz+IgD6c1Q2ymF8XwQAAQBaHntfAgBaHntfAwBaHntfBABaHntf',
-        'Zong Han',
+        widget.groupId,
         null,
         0);
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Video Call Test'),
-        ),
-        body: Stack(
-          children: [
-            Center(
-              child: _switch ? _renderRemoteVideo() : _renderLocalPreview(),
-            ),
-            Align(
-              alignment: Alignment.topLeft,
-              child: Container(
-                width: 100,
-                height: 100,
-                color: Colors.blue,
-                child: GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      _switch = !_switch;
-                    });
-                  },
-                  child: Center(
-                    child:
-                        _switch ? _renderLocalPreview() : _renderRemoteVideo(),
-                  ),
-                ),
-              ),
-            ),
+  /// Create agora sdk instance and initialze
+  Future<void> _initAgoraRtcEngine() async {
+    AgoraRtcEngine.create(APP_ID);
+    AgoraRtcEngine.enableVideo();
+  }
+
+  /// Add agora event handlers
+  void _addAgoraEventHandlers() {
+    AgoraRtcEngine.onError = (dynamic code) {
+      setState(() {
+        String info = 'onError: ' + code.toString();
+        _infoStrings.add(info);
+      });
+    };
+
+    AgoraRtcEngine.onJoinChannelSuccess =
+        (String channel, int uid, int elapsed) {
+      setState(() {
+        String info = 'onJoinChannel: ' + channel + ', uid: ' + uid.toString();
+        _infoStrings.add(info);
+      });
+    };
+
+    AgoraRtcEngine.onLeaveChannel = () {
+      setState(() {
+        _infoStrings.add('onLeaveChannel');
+        _users.clear();
+      });
+    };
+
+    AgoraRtcEngine.onUserJoined = (int uid, int elapsed) {
+      setState(() {
+        String info = 'userJoined: ' + uid.toString();
+        _infoStrings.add(info);
+        _users.add(uid);
+      });
+    };
+
+    AgoraRtcEngine.onUserOffline = (int uid, int reason) {
+      setState(() {
+        String info = 'userOffline: ' + uid.toString();
+        _infoStrings.add(info);
+        _users.remove(uid);
+      });
+    };
+
+    AgoraRtcEngine.onFirstRemoteVideoFrame =
+        (int uid, int width, int height, int elapsed) {
+      setState(() {
+        String info = 'firstRemoteVideo: ' +
+            uid.toString() +
+            ' ' +
+            width.toString() +
+            'x' +
+            height.toString();
+        _infoStrings.add(info);
+      });
+    };
+  }
+
+  /// Helper function to get list of native views
+  List<Widget> _getRenderViews() {
+    List<Widget> list = [AgoraRenderWidget(0, local: true, preview: true)];
+    _users.forEach((int uid) => {list.add(AgoraRenderWidget(uid))});
+    return list;
+  }
+
+  /// Video view wrapper
+  Widget _videoView(view) {
+    return Expanded(child: Container(child: view));
+  }
+
+  /// Video view row wrapper
+  Widget _expandedVideoRow(List<Widget> views) {
+    List<Widget> wrappedViews =
+        views.map((Widget view) => _videoView(view)).toList();
+    return Expanded(
+        child: Row(
+      children: wrappedViews,
+    ));
+  }
+
+  /// Video layout wrapper
+  Widget _viewRows() {
+    List<Widget> views = _getRenderViews();
+    switch (views.length) {
+      case 1:
+        return Container(
+            child: Column(
+          children: <Widget>[
+            _videoView(views[0]),
           ],
-        ),
+        ));
+      case 2:
+        return Container(
+            child: Column(
+          children: <Widget>[
+            _expandedVideoRow([views[0]]),
+            _expandedVideoRow([views[1]]),
+          ],
+        ));
+      case 3:
+        return Container(
+            child: Column(
+          children: <Widget>[
+            _expandedVideoRow(views.sublist(0, 2)),
+            _expandedVideoRow(views.sublist(2, 3)),
+          ],
+        ));
+      case 4:
+        return Container(
+            child: Column(
+          children: <Widget>[
+            _expandedVideoRow(views.sublist(0, 2)),
+            _expandedVideoRow(views.sublist(2, 4)),
+          ],
+        ));
+      default:
+    }
+    return Container();
+  }
+
+  /// Toolbar layout
+  Widget _toolbar() {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Container(
+            alignment: Alignment.topRight,
+            padding: EdgeInsets.symmetric(vertical: 48),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                RawMaterialButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: new Icon(
+                    Icons.exit_to_app,
+                    color: Colors.white,
+                    size: 20.0,
+                  ),
+                  shape: new CircleBorder(),
+                  elevation: 2.0,
+                  fillColor: Colors.blueAccent,
+                  padding: const EdgeInsets.all(12.0),
+                ),
+                RawMaterialButton(
+                  onPressed: () => _onToggleMute(),
+                  child: new Icon(
+                    muted ? Icons.mic : Icons.mic_off,
+                    color: muted ? Colors.white : Colors.blueAccent,
+                    size: 20.0,
+                  ),
+                  shape: new CircleBorder(),
+                  elevation: 2.0,
+                  fillColor: muted ? Colors.blueAccent : Colors.white,
+                  padding: const EdgeInsets.all(12.0),
+                ),
+                RawMaterialButton(
+                  onPressed: () => _onSwitchCamera(),
+                  child: new Icon(
+                    Icons.switch_camera,
+                    color: Colors.blueAccent,
+                    size: 20.0,
+                  ),
+                  shape: new CircleBorder(),
+                  elevation: 2.0,
+                  fillColor: Colors.white,
+                  padding: const EdgeInsets.all(12.0),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
 
-  Widget _renderLocalPreview() {
-    if (_joined) {
-      return RtcLocalView.SurfaceView();
-    } else {
-      return Text(
-        'Please join channel first',
-        textAlign: TextAlign.center,
-      );
-    }
+  /// Info panel to show logs
+  Widget _panel() {
+    return Container(
+        padding: EdgeInsets.symmetric(vertical: 48),
+        alignment: Alignment.bottomCenter,
+        child: FractionallySizedBox(
+          heightFactor: 0.5,
+          child: Container(
+              padding: EdgeInsets.symmetric(vertical: 48),
+              child: ListView.builder(
+                  reverse: true,
+                  itemCount: _infoStrings.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    if (_infoStrings.length == 0) {
+                      return null;
+                    }
+                    return Padding(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 3, horizontal: 10),
+                        child: Row(mainAxisSize: MainAxisSize.min, children: [
+                          Flexible(
+                              child: Container(
+                                  padding: EdgeInsets.symmetric(
+                                      vertical: 2, horizontal: 5),
+                                  decoration: BoxDecoration(
+                                      color: Colors.yellowAccent,
+                                      borderRadius: BorderRadius.circular(5)),
+                                  child: Text(_infoStrings[index],
+                                      style:
+                                          TextStyle(color: Colors.blueGrey))))
+                        ]));
+                  })),
+        ));
   }
 
-  Widget _renderRemoteVideo() {
-    if (_remoteUid != null) {
-      return RtcRemoteView.SurfaceView(uid: _remoteUid);
-    } else {
-      return Text(
-        'Please wait remote user join',
-        textAlign: TextAlign.center,
-      );
-    }
+  void _onCallEnd(BuildContext context) {
+    Navigator.pop(context);
+  }
+
+  void _onToggleMute() {
+    setState(() {
+      muted = !muted;
+    });
+    AgoraRtcEngine.muteLocalAudioStream(muted);
+  }
+
+  void _onSwitchCamera() {
+    AgoraRtcEngine.switchCamera();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.black,
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: Center(
+                  child: Stack(
+                //uncomment _panel for debugging
+                children: <Widget>[_panel(), _viewRows(), _toolbar()],
+              )),
+            ),
+            Expanded(
+                child: Container(
+              color: Colors.black,
+            ))
+          ],
+        ));
   }
 }
